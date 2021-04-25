ee2f3c26e9180644cb1a91f7316569b1045053a6
kbferrigan <kbferrigan@outlook.com>
2021-04-25 13:38:05 -0500

    init

---
+++ src/MoveToFront.java
@@ -0,0 +1,9 @@
+public class MoveToFront {
+    // apply move-to-front encoding, reading from stdin and writing to stdout
+    public static void encode()
+    // apply move-to-front decoding, reading from stdin and writing to stdout
+    public static void decode()
+    // if args[0] is "-", apply move-to-front encoding
+// if args[0] is "+", apply move-to-front decoding
+    public static void main(String[] args)
+}
