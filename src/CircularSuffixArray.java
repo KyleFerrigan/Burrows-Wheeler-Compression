a0ccd6d1bbd970f3147115b31a2255877fe9ac67
kbferrigan <kbferrigan@outlook.com>
2021-04-25 14:42:42 -0500

    init

--- src/CircularSuffixArray.java
+++ src/CircularSuffixArray.java
@@ -2,9 +2,15 @@ public class CircularSuffixArray {
     // circular suffix array of s
     public CircularSuffixArray(String s)
     // length of s
-    public int length()
+    public int length(){
+
+    }
     // returns index of ith sorted suffix
-    public int index(int i)
+    public int index(int i){
+
+    }
     // unit testing (required)
-    public static void main(String[] args)
+    public static void main(String[] args){
+
+    }
 }
--- src/MoveToFront.java
+++ src/MoveToFront.java
@@ -1,9 +1,15 @@
 public class MoveToFront {
     // apply move-to-front encoding, reading from stdin and writing to stdout
-    public static void encode()
+    public static void encode(){
+
+    }
     // apply move-to-front decoding, reading from stdin and writing to stdout
-    public static void decode()
+    public static void decode(){
+        
+    }
     // if args[0] is "-", apply move-to-front encoding
 // if args[0] is "+", apply move-to-front decoding
-    public static void main(String[] args)
+    public static void main(String[] args){
+
+    }
 }
