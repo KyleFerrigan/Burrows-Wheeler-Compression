8c85607b2ea5a6f069fa44d6d522bb7dba83f13a
pagalati <78822760+pagalati@users.noreply.github.com>
2021-04-30 15:57:04 -0500

    Update BurrowsWheeler.java
    Added functionality to inverseTransform and main

--- src/BurrowsWheeler.java
+++ src/BurrowsWheeler.java
@@ -2,6 +2,7 @@ public class BurrowsWheeler {
     private static int index;
     private static int origin;
     private static int prev;
+    private static int ascii = 256;
     // apply Burrows-Wheeler transform,
     // reading from standard input and writing to standard output
     public static void transform(){
@@ -32,14 +33,55 @@ public class BurrowsWheeler {
         // No longer using BinaryStd, close so we no longer write to it
         BinaryStdOut.close();
     }
+    
     // apply Burrows-Wheeler inverse transform,
-// reading from standard input and writing to standard output
+    // reading from standard input and writing to standard output
     public static void inverseTransform(){
-
+        origin = BinaryStdIn.readInt();
+        int[] count = new int[ascii+1];
+        String s = BinaryStdIn.readString();
+        // Iterate through the array, incrementing the count for each character
+        for (int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            count[c+1]++;
+        }
+        // Update the count
+        for (int j = 0; j < ascii; j++){
+            count[j+1] += count[ascii];
+        }
+        // Copy into a sorted array, while also keeping track of the next traversal
+        char[] temp = new char[s.length()];
+        int[] next = new int[s.length()];
+        for (int k = 0; k < s.length(); k++){
+            char cc = s.charAt(k);
+            temp[count[cc]] = cc;
+            next[count[cc]] = k;
+            count[cc]++;
+        }
+        // Rebuild the original string from the int[] next array
+        int first = origin;
+        for (int l = 0; l < s.length(); l++){
+            BinaryStdOut.write(temp[first]);
+            first = next[first];
+        }
+        // No longer using BinaryStd, close so we no longer write to it
+        BinaryStdOut.close();
     }
+    
     // if args[0] is "-", apply Burrows-Wheeler transform
-// if args[0] is "+", apply Burrows-Wheeler inverse transform
+    // if args[0] is "+", apply Burrows-Wheeler inverse transform
     public static void main(String[] args){
-
+        if (args == null) throw new java.lang.IllegalArgumentException("args is null");
+        String word = args[0];
+        switch (word) {
+            case "-":
+                BurrowsWheeler.transform();
+                break;
+            case "+":
+                BurrowsWheeler.inverseTransform();
+                break;
+            default:
+                throw new java.lang.IllegalArgumentException("word = " + word);
+        }
     }
 }
